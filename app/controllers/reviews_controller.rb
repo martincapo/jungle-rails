9006418e0ec3138042a7f62230f456a6ac6adb13
martincapo <martincapo@yahoo.com>
2017-10-13 17:22:26 +0000

    before_filter to protect all reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,14 @@
 class ReviewsController < ApplicationController
+  before_filter :require_login
 
   def create
     @product = Product.find(params[:product_id])
+    # after @review has been initialized, but before calling .save on it:
+    @review.user = current_user
 
     @review = Review.new(
       product_id: @product[:id],
-      user_id: session[:user_id],
+      user_id: @review.user,
       description: params[:review][:description],
       rating: params[:rating]
     )
@@ -14,4 +17,11 @@ class ReviewsController < ApplicationController
       redirect_to :back, notice: 'Review created!'
     end
   end
+
+  private
+
+  def require_login
+    redirect_to :root, notice: 'Please login or singin!' unless current_user
+  end
+
 end
