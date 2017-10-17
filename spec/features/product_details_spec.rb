fbf1ea4b70650ecace83ae49b3c93bc8856f4913
martincapo <martincapo@yahoo.com>
2017-10-17 17:56:06 +0000

    testing product page is done

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+  end
+
+  scenario "They see product details" do
+  # ACT
+    visit 'products/1'
+
+  # DEBUG
+    save_screenshot
+
+  # VERIFY
+   puts page.html
+
+
+  end
+end
