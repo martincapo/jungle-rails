edee845a02e2bc9d1e77206fad72f9b028452aa6
martincapo <martincapo@yahoo.com>
2017-10-17 20:39:29 +0000

    bug fix in product spec

--- spec/features/add_to_carts_spec.rb
+++ spec/features/add_to_carts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature, js: true do
-   # SETUP
+RSpec.feature "AddToCarts", type: :feature do
+ # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
 
@@ -16,17 +16,6 @@ RSpec.feature "ProductDetails", type: :feature, js: true do
     end
   end
 
-  scenario "They see product details" do
-  # ACT
-    visit root_path
-    page.first('.product').find_link('Details').click
 
-    expect(page).to have_content "Product Reviews"
-  # DEBUG
-    save_screenshot
 
-  # VERIFY
-    puts page.html
-
-  end
 end
